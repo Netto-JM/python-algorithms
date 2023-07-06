05b18acfe63c1b536d93d9363bcf4fcfc4bd8f19
NettoJM <nettojm86@gmail.com>
2023-07-06 15:48:45 -0300

    Implement study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    result = 0
+    if target_time is None or target_time <= 0:
+        return None
+    for period in permanence_period:
+        if any(not isinstance(x, int) for x in period):
+            return None
+        if period[0] <= target_time <= period[1]:
+            result += 1
+    return result
