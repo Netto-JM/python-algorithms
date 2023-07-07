c1190233123ae5f9cfae0afcad7e8dcfa67a8068
NettoJM <nettojm86@gmail.com>
2023-07-07 15:09:29 -0300

    Implement test_encrypt_message tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError,  match="tipo inválido para key"):
+        encrypt_message('valid', 'invalid')
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(42, 42)
+    with pytest.raises(TypeError,  match="tipo inválido para key"):
+        encrypt_message(24, 'invalid')
+    assert encrypt_message('my_message', 5) == 'em_ym_egass'
+    assert encrypt_message('my_message', 4) == 'egasse_m_ym'
+    assert encrypt_message('my_message', 17) == 'egassem_ym'
